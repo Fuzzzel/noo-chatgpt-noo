a7b3a4fd5c18b88de52d6e07eb04392233c9f889
Daniel Zbihlej <65666380+Fuzzzel@users.noreply.github.com>
2025-05-21 12:05:49 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+window.addEventListener('resize', () => {
+  resizeCanvas();
+});
+
+
 const keys = {
   ArrowLeft: false,
   ArrowRight: false,
